18f39df43fd088682fbdb6a4cb2de49c776cf371
Bruno Barros <bobarros@gmail.com>
2021-04-30 11:28:58 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,65 @@
+<p align="center">
+  <a href="https://brunobarros.dev">
+    <img alt="Bruno's Logo" src="https://brunobarros.dev/img/favicon.png" width="128" />
+  </a>
+</p> 
+<h3 align="center">:wave: Hi! I'm Bruno Barros</h2>
+<p align="center">A Web Developer</p>
+
+*Obs: If you're using this, would you be kind to tell me how useful it is to you?*
+
+## :rocket: Quick start
+
+**Running the function locally**
+
+### Step 1: Clone The Repo
+
+Fork the repository, then clone the repo locally by doing -
+
+```bash
+git clone https://github.com/bobarros/customcss-to-file
+```
+
+### Step 2: Install Dependencies
+
+cd into the directory
+
+```bash
+cd customcss-to-file
+```
+
+install all the dependencies
+```bash
+yarn install
+or
+npm install
+```
+
+### Step 3: Editing variables or not
+
+I'm using Google's website as a placeholder if you just want to test. /
+Replace with your target on getCSS.js, if you will.
+```
+const targetUrl = "https://www.google.com/";
+const targetElement = "k1zIA"; //id or classname
+```
+
+Now, jus execute it:
+
+```
+yarn start
+or maybe
+node getCSS.js
+```
+
+## Dependencies
+
+**1.** puppeteer
+
+## :v: Reach me
+
+Feel free to tell me about errors or improvements.
+
+## :hugs: Reach me
+
+I wish you all the best!
---
+++ getCSS.js
@@ -0,0 +1,105 @@
+const targetUrl = "https://www.google.com/";
+const targetElement = "k1zIA"; //id or classname
+
+////////////////////////////////////////////////
+
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+
+////////////////////////////////////////////////
+
+const getTrees = async () => {
+  //////// Cache config to make pupppeter faster
+  const browser = await puppeteer.launch({
+    userDataDir: "./cacheData",
+  });
+
+  //////// Alocating the new page
+  const page = await browser.newPage();
+
+  //////// Blocking images, videos and font files to make request faster
+  await page.setRequestInterception(true);
+  page.on("request", (request) => {
+    if (request.resourceType() === "image") {
+      request.abort();
+    } else if (request.resourceType() === "video") {
+      request.abort();
+    } else if (request.resourceType() === "font") {
+      request.abort();
+    } else {
+      request.continue();
+    }
+  });
+
+  //////// Opens targeted url
+  await page.goto(targetUrl);
+
+  const elementCSS = await page.evaluate((targetElement) => {
+
+    try {
+      //////////// Getting element
+      let target;
+      if (document.getElementById(targetElement)) {
+        target = document.getElementById(targetElement);
+      } else {
+        target = document.getElementsByClassName(targetElement)[0];
+      }
+
+      //////// Saving css computed with classes still active
+      const targetCSS = window.getComputedStyle(target);
+      let cssOne = {};
+      for (let i = 0; i < targetCSS.length; i++) {
+        const element = targetCSS[i];
+        cssOne = {
+          ...cssOne,
+          [element]: targetCSS[element]
+        };
+      }
+
+      //////// Saving css computed without classes
+      target.removeAttribute("class");
+      target.removeAttribute("id");
+      const targetCSSclean = window.getComputedStyle(target);
+      let cssTwo = {};
+
+      //////// Saving differences between versions
+      for (let i = 0; i < targetCSSclean.length; i++) {
+        const element = targetCSSclean[i];
+        if (!(targetCSSclean[element] === cssOne[element]) && !element.includes("webkit")) {
+          cssTwo = {
+            ...cssTwo,
+            [element]: cssOne[element] + "@#$"
+          };
+        }
+      }
+
+      //////// Making output look like a css file
+      cssTwo =
+        ".customComputedCSS " +
+        JSON.stringify(cssTwo)
+          .replace(/"|\\/g, " ")
+          .replace(/@#\$ ,/g, ";\n")
+          .replace("\{", "\{\n")
+          .replace("\}", "\n\}")
+          .replace("@#$", "");
+
+      //////// returning the file from puppeteer
+      return cssTwo;
+    } catch (error) {
+      return "Error in trying to find the element."
+    }
+
+  }, targetElement);
+
+  //////// closes virtual browser
+  browser.close();
+
+  //////// Saving CSS
+  await fs.writeFile("./computedCSS.css", elementCSS, function (err) {
+    if (err) return console.log("Error saving css! ", err);
+  });
+
+}
+
+getTrees();
+
